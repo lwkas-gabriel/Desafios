2dc1b74492eee2a9ef02ffb7b1b82387704b5dc5
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-07-06 00:02:57 -0300

    começando componente NewEntryButton

---
+++ corelab-web-challenge/src/components/NewEntryButton/index.tsx
@@ -0,0 +1,5 @@
+export function NewEntryButton(){
+    return (
+        <button id="buttonNewEntry" className="bg-[#65dcc7] text-[#020202] px-24 py-3 rounded-[100px] font-sans">ADICIONAR</button>
+    )
+}
